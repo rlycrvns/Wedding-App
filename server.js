21bf0a543287c704422026669b9ea3939c988957
Riley Cravens <rlycrvns@gmail.com>
2019-11-17 21:03:51 -0800

    edited serverjs to remove problematic code

--- server.js
+++ server.js
@@ -12,20 +12,9 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 //Use Routes
 app.use('/api/songs', songs);
 
-// Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // set static folder
-//   app.use('/', express.static(path.join(__dirname, '/client/build')));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build')); // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => {
