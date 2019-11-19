7893946bf3c5aa52eb58f5b0e3e0489efe36fbd7
Riley Cravens <rlycrvns@gmail.com>
2019-11-18 17:05:19 -0800

    updated styles

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -8,30 +8,46 @@ export default class Details extends Component {
         <div className='container'>
           <h1>Details</h1>
           <div className='info-body'>
-            <h2>When:</h2>
-            <h3>
-              <span>Arrival at 5:00 PM</span>
-              <span>Outdoor Ceremony at 5:30 PM</span>
-              <span>Reception to Follow</span>
-            </h3>
-            <h2>Where:</h2>
-            <h3>
-              <a href='https://goo.gl/maps/fRn59sfiuLAZudhV7'>
-                Scholl's Valley Lodge
-              </a>
-            </h3>
-            <h3>
-              <span>2840 SW River Road</span>
-              <span>Hillsboro, OR 97123</span>
-              <span>(Half hour from Downtown Portland)</span>
-            </h3>
-            <h2>Attire:</h2>
-            <h3>Semi-Formal</h3>
-            <h2>Food and Drink</h2>
-            <h3>
-              <span>Light Meal</span>
-              <span>Beer and Wine Bar</span>
-            </h3>
+            <div className='info-block'>
+              <h2>When:</h2>
+              <h3>
+                <span>September 6, 2020</span>
+                <span>Arrival at 5:00 PM</span>
+                <span>Outdoor Ceremony at 5:30 PM</span>
+                <span> On Premesis Reception to Follow</span>
+              </h3>
+            </div>
+            <div className='info-block'>
+              <h2>Where:</h2>
+              <h3>
+                <a href='https://goo.gl/maps/fRn59sfiuLAZudhV7'>
+                  Scholl's Valley Lodge
+                </a>
+              </h3>
+              <h3>
+                <span>2840 SW River Road</span>
+                <span>Hillsboro, OR 97123</span>
+                <span>(Half hour from Downtown Portland)</span>
+              </h3>
+            </div>
+            <div className='info-block'>
+              <h2>Attire:</h2>
+              <h3>Semi-Formal</h3>
+            </div>
+            <div className='info-block'>
+              <h2>Food and Drink</h2>
+              <h3>
+                <span>Light Meal</span>
+                <span>Beer and Wine Bar</span>
+              </h3>
+            </div>
+            <div className='info-block'>
+              <h2>Registry</h2>
+              <h3>
+                <span>Light Meal</span>
+                <span>Beer and Wine Bar</span>
+              </h3>
+            </div>
           </div>
         </div>
       </div>
