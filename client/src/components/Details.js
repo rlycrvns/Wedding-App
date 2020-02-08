25744313f4c4b41336257bcd8273685ef5e3b9af
Riley Cravens <rlycrvns@gmail.com>
2020-02-08 13:06:12 -0800

    added info to detail page

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -26,12 +26,10 @@ export default class Details extends Component {
             <div className='info-body'>
               <div className='info-block'>
                 <h2>When:</h2>
-                <h3>
-                  <span>September 6, 2020</span>
-                  <span>Arrival at 5:00 PM</span>
-                  <span>Outdoor Ceremony at 5:30 PM</span>
-                  <span> On-site Reception to Follow</span>
-                </h3>
+                <h3>September 6, 2020</h3>
+                <h3>Arrival at 5:00 PM</h3>
+                <h3>Outdoor Ceremony at 5:30 PM</h3>
+                <h3>Outdoor Reception to Follow On-site</h3>
               </div>
               <div className='info-block'>
                 <h2>Where:</h2>
@@ -40,30 +38,46 @@ export default class Details extends Component {
                     Scholl's Valley Lodge
                   </a>
                 </h3>
+                <h3>2840 SW River Road</h3>
+                <h3>Hillsboro, OR 97123</h3>
+                <h3>(45 minutes from Downtown Portland)</h3>
+                <h3>Parking is limited, please carpool if possible</h3>
+              </div>
+              <div className='info-block'>
+                <h2>Accessibility</h2>
+                <h3>Gravel Parking Lot</h3>
                 <h3>
-                  <span>2840 SW River Road</span>
-                  <span>Hillsboro, OR 97123</span>
-                  <span>(Half hour from Downtown Portland)</span>
+                  Ceremony and Reception Site are accessible without stairs
                 </h3>
               </div>
               <div className='info-block'>
-                <h2>Attire:</h2>
-                <h3>Semi-Formal</h3>
-              </div>
-              <div className='info-block'>
-                <h2>Food and Drink</h2>
+                <h2>Info</h2>
                 <h3>
-                  <span>Light Meal</span>
-                  <span>Beer and Wine Bar</span>
+                  <span>Attire:</span>
                 </h3>
+                <h4>Semi-Formal</h4>
+                <h4>Ceremony will be held on a lawn, beware of heels</h4>
+                <h3>
+                  <span>Food and Drink:</span>
+                </h3>
+                <h4>Appetizers followed by Light Meal</h4>
+                <h4>Beer and wine bar</h4>
+                <h3>
+                  <span>Children:</span>
+                </h3>
+                <h4>
+                  To allow all guests to relax and enjoy themselves, we have
+                  chosen to make our special day 21+ only. We thank you for your
+                  understanding.
+                </h4>
+                <h3>
+                  <span>Unplugged Ceremony:</span>
+                </h3>
+                <h4>
+                  We invite you to be fully present. Kindly turn off all devices
+                  and enjoy this special moment with us.
+                </h4>
               </div>
-              {/* <div className='info-block'>
-              <h2>Registry</h2>
-              <h3>
-                <span>Light Meal</span>
-                <span>Beer and Wine Bar</span>
-              </h3>
-            </div> */}
             </div>
           </div>
         </div>
